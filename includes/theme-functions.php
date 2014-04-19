9805fd127f46a6dabb9e5cffdd386517cac2367c
Scot Rumery <scot@rumspeed.com>
2014-04-19 08:49:44 -0400

    add_theme_support for post-formats

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -9,3 +9,15 @@
 define( 'RUM_THEME_ID', 'luna-blue' );
 define( 'RUM_THEME_NAME', 'Luna Blue' );
 
+
+
+
+// setup Luna Blue
+function luna_blue_setup() {
+
+	// Post Formats
+	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
+
+}
+add_action( 'after_setup_theme', 'luna_blue_setup' );
+
