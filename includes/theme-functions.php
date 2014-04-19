f16ae8c0cf4ac393c66aa8b4241b33bb7038c5bd
Scot Rumery <scot@rumspeed.com>
2014-04-19 08:36:52 -0400

    Define theme constants

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -3,3 +3,9 @@
 /* Theme functions
 /*-------------------------------------------------------*/
 
+
+
+// Define theme constants
+define( 'RUM_THEME_ID', 'luna-blue' );
+define( 'RUM_THEME_NAME', 'Luna Blue' );
+
